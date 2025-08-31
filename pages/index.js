7393c7928bb0ccbd2bf3e615bd40c77439e7b6d8
Culture-Hub-Com <mark.holson@culture-hub.com>
2025-08-31 12:19:49 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      minHeight: "100vh",
+      background: "black",
+      color: "white",
+      fontFamily: "sans-serif",
+      flexDirection: "column"
+    }}>
+      <h1>CultureHub Training Platform</h1>
+      <p>If you can see this, Next.js is working!</p>
+    </div>
+  );
+}
